952196a57d0e3e63356a65ed907a77f7e7b23a3e
yyeonjju <llilyha123@gmail.com>
2024-07-03 15:01:27 +0900

    Initial Commit

---
+++ ReminderProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderProject
+//
+//  Created by 하연주 on 7/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
